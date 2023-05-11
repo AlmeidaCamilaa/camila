1ae1a003bb2ee5711618705939a09d1288e68b0a
AlmeidaCamilaa <124744336+AlmeidaCamilaa@users.noreply.github.com>
2023-05-11 20:29:24 -0300

    aula11\05

---
+++ Componentes/apostila_03/index.js
@@ -0,0 +1,31 @@
+import  React, { useState} from 'react'; 
+import { Text, View , TouchableOpacity } from 'react-native';
+
+import styles from './styles';
+
+
+export default function apostila03() {
+
+  const[contador, setContador] = useState(0);
+
+ function AddNumber(){
+  setContador(contador + 1);
+
+ }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>Exemplo apostila 3</Text>
+
+      <Text style={styles.textCounter}>{contador}</Text>
+
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={ () => AddNumber()}
+      >
+        <Text style={styles.textButton}>Somar +1</Text>
+      </TouchableOpacity>
+    
+    </View>
+
+  );
+}
