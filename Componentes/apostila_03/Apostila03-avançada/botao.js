ec29d387542d8f47caea8f6c5645735988cf815d
AlmeidaCamilaa <124744336+AlmeidaCamilaa@users.noreply.github.com>
2023-05-16 21:38:26 -0300

    aula avancada3

--- Componentes/apostila_03/Apostila03-avançada/botao.js
+++ Componentes/apostila_03/Apostila03-avançada/botao.js
@@ -5,13 +5,12 @@ import styles from './styles';
 function Botao({sinal,funcao}) {
     return(
         <TouchableOpacity
-        style={[styles.button, styles.boaoSinal]}
-        onPress={() => funcao()}
-        >
-
-        <Text style={sttyles.textButton} >{Sinal}</Text>
+            style={[styles.button, styles.boaoSinal]}
+            onPress={() => funcao()}
+    >
+            <Text style={sttyles.textButton} >{Sinal}</Text>
         </TouchableOpacity>
     );
-    }
+}
 
     export default Botao;
