b2e163cdf6c2c4b889a042f34a60192b549ffdbf
AlmeidaCamilaa <124744336+AlmeidaCamilaa@users.noreply.github.com>
2023-05-16 21:30:56 -0300

    Apostila_03_avancado

---
+++ Componentes/apostila_03/Apostila03-avançada/botao.js
@@ -0,0 +1,17 @@
+import { Text, TouchableOpacity } from 'react-native';
+
+import styles from './styles';
+
+function Botao({sinal,funcao}) {
+    return(
+        <TouchableOpacity
+        style={[styles.button, styles.boaoSinal]}
+        onPress={() => funcao()}
+        >
+
+        <Text style={sttyles.textButton} >{Sinal}</Text>
+        </TouchableOpacity>
+    );
+    }
+
+    export default Botao;
